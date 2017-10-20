2263a4a9d2572f1ce50cd01c60680078508b56d8
Maxime Temmerier <maxime-temmerier@etud.univ-tln.fr>
2017-10-20 23:10:39 +0200

    [gitflow]squashing 'feature-cards' into 'development'

---
+++ src/main/java/cards/Card.java
@@ -0,0 +1,24 @@
+package cards;
+
+public class Card {
+    String name;
+    String image;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+
+}
