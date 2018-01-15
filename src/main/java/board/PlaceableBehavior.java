e2c3286d5e858e30c976d6927d96879abbf4e479
Lois Tonnet <tonnetlois@gmail.com>
2018-01-15 22:53:19 +0100

    Added JPA annotations to PlaceableBehavior

--- src/main/java/board/PlaceableBehavior.java
+++ src/main/java/board/PlaceableBehavior.java
@@ -1,21 +1,42 @@
 package board;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import java.io.Serializable;
 
+@Entity
 public class PlaceableBehavior  implements Serializable {
-        private Square square;
+    @Id
+    int id;
 
-        public PlaceableBehavior() {}
+    @OneToOne
+    private Square square;
 
-        public PlaceableBehavior(Square square) {
+    public PlaceableBehavior() {}
+
+    public PlaceableBehavior(Square square) {
                 this.square = square;
         }
 
-        public Square getSquare() {
+    public PlaceableBehavior(int id, Square square) {
+        this.id = id;
+        this.square = square;
+    }
+
+    public Square getSquare() {
                 return square;
         }
 
-        public void setSquare(Square square) {
+    public void setSquare(Square square) {
                 this.square = square;
         }
+
+    public int getId() {
+        return id;
+    }
+
+    public int setId(int id) {
+        this.id = id;
+    }
 }
